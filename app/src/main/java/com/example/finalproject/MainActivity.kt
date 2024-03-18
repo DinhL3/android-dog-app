acfb81161c68e6620c83e2324d13d54a38a61a39
Dinh Le <lqdquangdinh@gmail.com>
2024-03-18 20:00:13 +0200

    add home screen buttons and icons

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -3,20 +3,31 @@ package com.example.finalproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddAPhoto
+import androidx.compose.material.icons.filled.Pets
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.finalproject.ui.theme.CreamWhite
+import com.example.finalproject.ui.theme.DarkOrange
 import com.example.finalproject.ui.theme.FinalProjectTheme
+import com.example.finalproject.ui.theme.OliveGreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +37,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = CreamWhite
                 ) {
                     HomeScreen()
                 }
@@ -37,20 +48,54 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreen() {
-    Column(modifier = Modifier.fillMaxSize().padding(0.dp),
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Dinh's dog app")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(0.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Dinh's Dog App \uD83D\uDC36",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = OliveGreen,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
         Button(
             onClick = { },
-            modifier = Modifier.padding(bottom = 16.dp)
-        ) { Text(text = "See dog breeds") }
+            modifier = Modifier.padding(bottom = 8.dp),
+            colors = ButtonDefaults.buttonColors(DarkOrange)
+        ) {
+            Icon(
+                Icons.Filled.Pets,
+                contentDescription = "Pets",
+                modifier = Modifier.size(ButtonDefaults.IconSize),
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Text(text = "See dog breeds")
+        }
+        Button(
+            onClick = { },
+            modifier = Modifier.padding(bottom = 0.dp),
+            colors = ButtonDefaults.buttonColors(DarkOrange)
+
+        ) {
+            Icon(
+                Icons.Filled.AddAPhoto,
+                contentDescription = "Add picture",
+                modifier = Modifier.size(ButtonDefaults.IconSize),
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Text(text = "Take a picture of your dog")
+        }
     }
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    FinalProjectTheme {
-//        Greeting("Android")
-//    }
-//}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FinalProjectTheme {
+        HomeScreen()
+    }
+}
